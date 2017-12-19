c979c1e688b047901216f1603468ba0d849f3203
Tzu Chao <chao.tzu5@gmail.com>
2017-12-19 17:55:12 -0500

    test for a bean class

--- reimbursement/src/main/java/com/revature/reimbursement/App.java
+++ reimbursement/src/main/java/com/revature/reimbursement/App.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 
 public class App 
 {
-	public static EmployeeDAO EDAO=new EmployeeDAO_jdbc();
-	public static ReimbursementDAO RDAO=new ReimbursementDAO_jdbc();
     public static void main( String[] args )
     {
+    	EmployeeDAO EDAO=new EmployeeDAO_jdbc();
+    	ReimbursementDAO RDAO=new ReimbursementDAO_jdbc();
     	Scanner sc=new Scanner(System.in);
     	String username, password;
     	do {
@@ -18,32 +18,34 @@ public class App
             username = sc.nextLine();
             System.out.print("Enter Password: ");
             password = sc.nextLine();
-    	}while(!verify_user(username,password));
+    	}while(!verify_user(username,password,EDAO));
     	Employee current_user=EDAO.getEmployee(username);
     	if(current_user.isAdmin()){
-    		display_admin_view(current_user);
+    		System.out.println("Hello! Manager " + current_user.getFirstName()+" "+ current_user.getLastName());
+    		display_admin_view(current_user, RDAO);
     	}else {
-    		display_user_view(current_user);
+    		System.out.println("Hello!" + current_user.getFirstName()+" "+ current_user.getLastName());
+    		display_user_view(current_user, RDAO);
     	}
-    	System.out.println("Enter 1 to add a reimbursement request, anything else to terminate the program");
+    	System.out.println("Enter 1 to add a reimbursement request, any other number to terminate the program");
     	System.out.println("What would you like to do?");
     	int command=sc.nextInt();
-    	process_command(command,current_user,sc);
+    	process_command(command,current_user,sc,RDAO);
     	System.out.println("Thank you for using the ERS.");
     	sc.close();
     }
     
-	private static void process_command(int command, Employee current_user, Scanner sc) {
+	private static void process_command(int command, Employee current_user, Scanner sc, ReimbursementDAO RDAO) {
 		switch(command) {
 			case 1:
-				add_reimbursement_requerst(current_user, sc);
+				add_reimbursement_requerst(current_user, sc, RDAO);
 				break;
 			default:
 				break;
 		}
 		
 	}
-	private static void add_reimbursement_requerst(Employee current_user, Scanner sc) {
+	private static void add_reimbursement_requerst(Employee current_user, Scanner sc, ReimbursementDAO RDAO) {
 		int e_id=current_user.getId();
 		double amount=0.0;
 		String purpose;
@@ -56,7 +58,7 @@ public class App
 	}
 
 	
-	private static void display_user_view(Employee current_user) {
+	private static void display_user_view(Employee current_user,ReimbursementDAO RDAO) {
 		List<Reimbursement> requests= RDAO.getEmployeeRequest(current_user.getId());
 		System.out.printf("|%10s||%10s||%10s||%20s||%10s|\n","r_id","employeeid","amount","purpose","status");
 		for(Reimbursement r : requests) {
@@ -65,7 +67,7 @@ public class App
 		}
 	}
 
-	private static void display_admin_view(Employee current_user) {
+	private static void display_admin_view(Employee current_user,ReimbursementDAO RDAO) {
 		List<Reimbursement> requests= RDAO.getAllRequest();
 		System.out.printf("|%10s||%10s||%10s||%20s||%10s|\n","r_id","employeeid","amount","purpose","status");
 		for(Reimbursement r : requests) {
@@ -74,7 +76,7 @@ public class App
 		
 	}
 
-	static boolean verify_user(String username, String password) {
+	static boolean verify_user(String username, String password,EmployeeDAO EDAO) {
 		Employee em=EDAO.getEmployee(username);
 		return password.equals(em.getPassWord());
 	}
--- reimbursement/src/test/java/com/revature/reimbursement/AppTest.java
+++ reimbursement/src/test/java/com/revature/reimbursement/AppTest.java
@@ -2,33 +2,47 @@ package com.revature.reimbursement;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import com.revature.reimbursement.App;
 public class AppTest {
 	public class EDAODummy implements EmployeeDAO{
-
+		public List<Employee> dummy_record=new ArrayList<>();
 		@Override
 		public void userRegistration(String firstName, String lastName, String userName, String passWord) {
-			// TODO Auto-generated method stub
-			
+			Employee em = new Employee();
+			em.setFirstName(firstName);
+			em.setLastName(lastName);
+			em.setUserName(userName);
+			em.setPassWord(passWord);
+			em.setAdmin(true);
+			dummy_record.add(em);
 		}
 
 		@Override
 		public Employee getEmployee(int id) {
-			// TODO Auto-generated method stub
 			return null;
 		}
 
 		@Override
 		public Employee getEmployee(String userName) {
-			// TODO Auto-generated method stub
-			return null;
+			Employee target=null;
+			for(Employee em : dummy_record) {
+				if(em.getUserName().equals(userName)) {
+					target=em;
+				}
+			}
+			return target;
 		}
 		
 	}
+	public EmployeeDAO DAO= new EDAODummy(); 
 	@Before
 	public void setUp() throws Exception {
+		
 	}
 
 	@Test
